b9c076e0e69bd29ab4adf50f8f84b73aa51c5241
Pavel Likin <pavellikin@gmail.com>
2020-10-21 18:08:20 +0300

    Initial commit.

---
+++ src/main/java/org/me/StreamToS3Issue.java
@@ -0,0 +1,132 @@
+package org.me;
+
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.server.HttpServerRequest;
+import reactor.netty.http.server.HttpServerResponse;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
+
+public class StreamToS3Issue {
+
+    private static final Logger log = LoggerFactory.getLogger(StreamToS3Issue.class);
+    private final S3AsyncClient s3AsyncClient;
+    private final HttpServer httpServer;
+
+    private DisposableServer disposableServer;
+
+    public AtomicInteger sequence = new AtomicInteger(1);
+
+    public StreamToS3Issue(String url, boolean checksumValidation) {
+        try {
+            this.s3AsyncClient = createS3AsyncClient(url, checksumValidation);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        var handler = createFileHandler();
+        this.httpServer = createHttpServer(handler);
+    }
+
+    /**
+     * Start HTTP server.
+     */
+    public synchronized void start() {
+        if (this.disposableServer == null) {
+            this.disposableServer = this.httpServer.bindNow();
+        }
+    }
+
+    /**
+     * Stop HTTP server.
+     */
+    public synchronized void stop() {
+        if (this.disposableServer != null) {
+            this.disposableServer.disposeNow();
+            this.disposableServer = null;
+        }
+    }
+
+    /**
+     * Configure S3AsyncClient.
+     * @param url s3 url.
+     * @param checksumValidation enable flag for checksum validation feature.
+     * @return S3AsyncClient.
+     * @throws URISyntaxException in case broken url.
+     */
+    private S3AsyncClient createS3AsyncClient(String url, boolean checksumValidation) throws URISyntaxException {
+        var credentials = AwsBasicCredentials.create("test", "test");
+        var s3Url = new URI(url);
+        return S3AsyncClient.builder()
+            .credentialsProvider(() -> credentials)
+            .region(Region.EU_WEST_2)
+            .endpointOverride(s3Url)
+            .serviceConfiguration(
+                S3Configuration.builder()
+                    .checksumValidationEnabled(checksumValidation)
+                    .build()
+            )
+            .httpClient(
+                NettyNioAsyncHttpClient.builder()
+                    .build()
+            )
+            .build();
+    }
+
+    /**
+     * Create callback with file streaming to s3.
+     * @return callback.
+     */
+    private BiFunction<? super HttpServerRequest, ? super HttpServerResponse, ? extends Publisher<Void>> createFileHandler() {
+        return (request, response) -> {
+            log.info("process.in");
+            // prepare s3 request
+            var s3Request = PutObjectRequest.builder()
+                .bucket("test-bucket")
+                .key(Integer.toString(sequence.getAndIncrement()))
+                .contentLength(Long.parseLong(request.requestHeaders().get("Content-Length")))
+                .build();
+            // prepare flux with ByteBuffer
+            var byteBufferFlux = request.receive().asByteBuffer();
+            var s3RequestBody = AsyncRequestBody.fromPublisher(byteBufferFlux);
+            return Mono.fromCompletionStage(() -> s3AsyncClient.putObject(s3Request, s3RequestBody)) //upload ByteBuffers to s3
+                .flatMap(r -> {
+                    log.info("process.out");
+                    return response.status(200)
+                        .then();
+                })
+                .onErrorResume(e -> {
+                    log.error("process.error", e);
+                    return response.status(500)
+                        .sendString(Mono.just(e.getMessage()))
+                        .then();
+                });
+        };
+    }
+
+    /**
+     * Configure reactor netty HTTP server.
+     * @param fileHandler callback with logic
+     * @return HttpServer
+     */
+    private HttpServer createHttpServer(BiFunction<? super HttpServerRequest, ? super HttpServerResponse, ? extends Publisher<Void>> fileHandler) {
+        return HttpServer.create()
+            .host("localhost")
+            .port(8080)
+            .route(routes -> routes.post("/file", fileHandler));
+    }
+
+}
---
+++ src/test/java/org/me/StreamToS3IssueTest.java
@@ -0,0 +1,172 @@
+package org.me;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import reactor.core.publisher.Mono;
+import reactor.netty.ByteBufFlux;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.client.HttpClientResponse;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.util.Objects.nonNull;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Expect content in all tests successfully uploaded and has the same bytes.
+ */
+public class StreamToS3IssueTest {
+
+    private static final LoggerContext ctx = (LoggerContext) LoggerFactory.getILoggerFactory();
+    // Test container with mock s3 implementation.
+    private static final GenericContainer<?> container = new GenericContainer<>("adobe/s3mock")
+        .withExposedPorts(9090)
+        .waitingFor(new LogMessageWaitStrategy().withRegEx("^.*Started S3MockApplication in.*"));
+
+    static {
+        ctx.getLogger("ROOT").setLevel(Level.INFO);
+    }
+
+    private final S3Client s3Client = getClient();
+
+    private StreamToS3Issue issue;
+
+    @BeforeAll
+    static void beforeAll() {
+        container.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        container.stop();
+    }
+
+    @BeforeEach
+    void setUp() {
+        s3Client.createBucket(b -> b.bucket("test-bucket"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (nonNull(issue)) {
+            issue.stop();
+        }
+        // clear all created files and buckets
+        var buckets = s3Client.listBuckets().buckets();
+        for (var bucket : buckets) {
+            var objects = s3Client.listObjects(b -> b.bucket(bucket.name())).contents();
+            for (var object : objects) {
+                s3Client.deleteObject(b -> b
+                    .bucket(bucket.name())
+                    .key(object.key()));
+            }
+            s3Client.deleteBucket(b -> b.bucket(bucket.name()));
+        }
+    }
+
+    @Test
+    public void streamWithChecksumValidation1Mb() {
+        startIssue(true);
+        var bytes = generateBytes(1_000_000);
+
+        var response = sendFile(bytes);
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        var s3Bytes = s3Client.getObjectAsBytes(b -> b
+            .bucket("test-bucket")
+            .key(Integer.toString(issue.sequence.get() - 1))
+        ).asByteArray();
+        assertEquals(bytes.length, s3Bytes.length);
+        assertArrayEquals(bytes, s3Bytes);
+    }
+
+    @Test
+    public void streamWithoutChecksumValidation1Mb() {
+        startIssue(false);
+        var bytes = generateBytes(1_000_000);
+
+        var response = sendFile(bytes);
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        var s3Bytes = s3Client.getObjectAsBytes(b -> b
+            .bucket("test-bucket")
+            .key(Integer.toString(issue.sequence.get() - 1))
+        ).asByteArray();
+        assertEquals(bytes.length, s3Bytes.length);
+        assertArrayEquals(bytes, s3Bytes);
+    }
+
+    @Test
+    public void streamWithChecksumValidation100Kb() {
+        startIssue(true);
+        var bytes = generateBytes(100_000);
+
+        var response = sendFile(bytes);
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        var s3Bytes = s3Client.getObjectAsBytes(b -> b
+            .bucket("test-bucket")
+            .key(Integer.toString(issue.sequence.get() - 1))
+        ).asByteArray();
+        assertEquals(bytes.length, s3Bytes.length);
+        assertArrayEquals(bytes, s3Bytes);
+    }
+
+    private void startIssue(boolean checksumValidation) {
+        issue = new StreamToS3Issue(getUrl(), checksumValidation);
+        issue.start();
+    }
+
+    private byte[] generateBytes(int size) {
+        var bytes = new byte[size];
+        ThreadLocalRandom.current().nextBytes(bytes);
+        return bytes;
+    }
+
+    private HttpClientResponse sendFile(byte[] bytes) {
+        return HttpClient.create()
+            .headers(h -> h
+                .set(HttpHeaderNames.CONTENT_TYPE, "application/json")
+                .set(HttpHeaderNames.CONTENT_LENGTH, bytes.length)
+            )
+            .baseUrl("localhost:8080")
+            .post()
+            .uri("/file")
+            .send(ByteBufFlux.fromInbound(Mono.just(bytes)))
+            .response()
+            .block();
+    }
+
+    private String getUrl() {
+        return String.format("http://%s:%d", container.getContainerIpAddress(), container.getMappedPort(9090));
+    }
+
+    private S3Client getClient() {
+        try {
+            return S3Client.builder()
+                .region(Region.EU_WEST_2)
+                .endpointOverride(new URI(getUrl()))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
+                .build();
+        } catch (URISyntaxException var1) {
+            throw new RuntimeException(var1);
+        }
+    }
+}
